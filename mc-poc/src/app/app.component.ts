e216b5e9d46a3e88b26a2c9ccf0b43957c2350e7
Radu Petrusan <radu.petrusan@yahoo.com>
2017-10-10 22:06:55 +0300

    firebase works

--- mc-poc/src/app/app.component.ts
+++ mc-poc/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+//import { AngularFire, AuthProviders, AuthMethods, FirebaseListObservable } from 'angularfire2';
+import { AngularFireDatabase, AngularFireDatabaseModule, AngularFireList } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from "rxjs/Observable";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  items: Observable<any>;
+  itemsToAdd: AngularFireList<any>;
+  name: any;
+  msgVal: string = '';
+
+
+  constructor(public db: AngularFireDatabase, public afAuth: AngularFireAuth) {
+    this.items = db.list('messages').valueChanges();
+    this.itemsToAdd = db.list('messages');
+
+    this.name = afAuth.authState;
+  }
+
+  login() {
+    this.afAuth.auth.signInAnonymously();
+  }
+
+  Send(desc: string) {
+    this.itemsToAdd.push({ message: desc });
+    this.msgVal = '';
+  }
 }
--- mc-poc/src/app/app.module.ts
+++ mc-poc/src/app/app.module.ts
@@ -2,8 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireDatabaseModule, AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBhMfBKF1yNxt4ZgjWlO4pzcuV1B7-yP3w",
+    authDomain: "mcpocdeva.firebaseapp.com",
+    databaseURL: "https://mcpocdeva.firebaseio.com",
+    projectId: "mcpocdeva",
+    storageBucket: "",
+    messagingSenderId: "1085237414328"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +24,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
